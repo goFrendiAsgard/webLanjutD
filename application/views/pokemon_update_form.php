b12e7879a8200930b86b3d5022199d60057d7490
lulukM <luluk.maslucha27@gmail.com>
2016-10-10 13:54:31 +0700

    Update pokemon_update_form.php

--- application/views/pokemon_update_form.php
+++ application/views/pokemon_update_form.php
@@ -2,3 +2,9 @@
 /* Buat form untuk update dengan metode post, yang akan dikirim ke pokemon/update_action.
  * Isi setiap input dengan data lama, sehingga user hanya perlu mengubah data yang perlu diubah saja
  */
+?>
+<form name="frm_update_pokemon" method='POST' action="<?=site_url('pokemon/update_action')?>">
+ <input type="text" name="nama" value="<?php echo $result['nama']; ?>"><br>
+  <input type="text" name="tipe" value ="<?php echo $result['tipe']; ?>"><br>
+  <button name="edit"> Update Pokemon </button>
+</form>
